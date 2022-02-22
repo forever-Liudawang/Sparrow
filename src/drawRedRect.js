3e41d04bb7355215f48b59d62d6bc2a202439662
andyLiu <lmy15973914530@163.com>
2022-02-22 16:33:37 +0800

    chore: add ci

--- src/drawRedRect.js
+++ src/drawRedRect.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export function drawRedRect(svg) {
-  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+  let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
   rect.setAttribute('x', 0);
   rect.setAttribute('y', 0);
   rect.setAttribute('fill', 'red');
